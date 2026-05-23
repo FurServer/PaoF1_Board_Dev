6f3bcdc32f8b5bcd3cd814151c61f2ea12b20e11
FurServer <furserver@qq.com>
2026-05-23 08:24:34 +0800

    完善switch_i2c1函数使用说明

--- Lib/Switch_I2C1/switch_i2c1.h
+++ Lib/Switch_I2C1/switch_i2c1.h
@@ -12,6 +12,24 @@ typedef enum
     I2C1_ON
 } I2C1_State;
 
+/**
+ * @brief  使能或禁用I2C1
+
+ * @param  stat 状态 I2C1_ON / I2C1_OFF
+ *
+ * @retval None
+ *
+ * @note STM32F103系列48pin芯片设计缺陷\n
+ *       当使能I2C1时,\n
+ *       PB5(SMBA)只能作为GPIO使用\n
+ *       受影响的功能: \n
+ *       SPI1重映射(MOSI)\n
+ *       TIM3CH2重映射\n\n
+ *       函数通过使能或禁用I2C1时钟\n
+ *       实现与其他功能分时复用\n
+ *       __HAL_RCC_I2C1_CLK_ENABLE();\n
+ *       __HAL_RCC_I2C1_CLK_DISABLE();
+ */
 void switch_i2c1(uint8_t stat);
 
 
