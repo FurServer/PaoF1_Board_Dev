0ff52d991a57e97fde3e8f15f3f43a63a4c17b98
Async异步泡 <furserver@qq.com>
2026-05-24 03:57:04 +0800

    修改注释

--- Lib/Median_Lowpass/median_lowpass.c
+++ Lib/Median_Lowpass/median_lowpass.c
@@ -17,19 +17,19 @@ int32_t median_lowpass(median_lp_t* filter, int32_t new_raw)
     int32_t min = INT32_MAX;
     const uint8_t n = filter->median_n;
 
-    /* 1. 环形填充采样缓冲区 */
+    /* 环形填充采样缓冲区 */
     filter->raw_buf[filter->buf_index] = new_raw;
     filter->buf_index = (filter->buf_index + 1) % n;
     if (filter->buf_filled < n) { filter->buf_filled++; }
 
-    /* 2. 缓冲区未满时，直接返回原始值以避免启动延迟 */
+    /* 缓冲区未满时,直接返回原始值以避免启动延迟 */
     if (filter->buf_filled < n)
     {
         filter->last_output = (float)new_raw;
         return new_raw;
     }
 
-    /* 3. 中位值平均：去掉最大最小值 */
+    /* 去掉最大最小值 */
     for (uint8_t i = 0; i < n; i++)
     {
         const int32_t val = filter->raw_buf[i];
@@ -40,7 +40,7 @@ int32_t median_lowpass(median_lp_t* filter, int32_t new_raw)
     sum = sum - max - min;
     const float median_val = (float)(sum) / (n - 2);
 
-    /* 4. 一阶低通滤波 */
+    /* 一阶低通滤波 */
     filter->last_output += filter->lp_alpha * (median_val - filter->last_output);
 
     return (int32_t)filter->last_output;
--- Lib/Median_Lowpass/median_lowpass.h
+++ Lib/Median_Lowpass/median_lowpass.h
@@ -5,8 +5,8 @@
 
 typedef struct
 {
-    uint8_t median_n; // 中位值窗口，建议5~9的奇数
-    float lp_alpha; // 低通系数，0~1，越小越平滑
+    uint8_t median_n; // 中位值窗口, 建议5~9的奇数
+    float lp_alpha; // 低通系数, 0~1, 越小越平滑
     float last_output; // 低通滤波器的上次输出
     int32_t* raw_buf; // 指向内部缓冲区
     uint8_t buf_index; // 环形指针
@@ -18,8 +18,8 @@ typedef struct
  * @brief 声明并初始化滤波器
  *
  * @param   name      滤波器变量名
- * @param   n         中位值窗口，建议5~9的奇数
- * @param   alpha     低通系数，0~1，越小越平滑
+ * @param   n         中位值窗口, 建议5~9的奇数
+ * @param   alpha     低通系数, 0~1, 越小越平滑
  *
  */
 #define MEDIAN_LP_DEFINE(name, n, alpha)                    \
@@ -31,12 +31,12 @@ typedef struct
  * @brief 运行时初始化滤波器实例
  *
  * @param   filter     指向滤波器状态结构体指针
- * @param   median_n   中位值窗口，建议5~9的奇数
- * @param   lp_alpha   低通系数，0~1，越小越平滑
- * @param   buf        缓冲区指针，数组长度需与 median_n 相同
+ * @param   median_n   中位值窗口, 建议5~9的奇数
+ * @param   lp_alpha   低通系数, 0~1, 越小越平滑
+ * @param   buf        缓冲区指针, 数组长度需与 median_n 相同
  *
- * @note    多数场景推荐使用 MEDIAN_LP_DEFINE() 一行声明。
- *          仅在需要运行时动态决定参数时使用本函数：
+ * @note    编译时推荐使用 MEDIAN_LP_DEFINE()\n
+ *          仅在需要运行中动态决定参数时使用本函数:
  * @code
  * int32_t buf[n] = {0};
  * median_lp_t filter = {0};
@@ -46,14 +46,14 @@ typedef struct
 void median_lp_init(median_lp_t* filter, uint8_t median_n, float lp_alpha, int32_t* buf);
 
 /**
- * @brief 中位值低通滤波（去极值平均 + 一阶低通）
+ * @brief 中位值低通滤波 (去极值平均 + 一阶低通)
  *
  * @param   filter     指向滤波器状态结构体指针
  * @param   new_raw    新数据
  *
  * @retval  int32_t    滤波后的输出值
  *
- * @note    编译初始化可使用 MEDIAN_LP_DEFINE()\n
+ * @note    编译初始化推荐使用 MEDIAN_LP_DEFINE()\n
  *          运行时初始化可使用 median_lp_init()\n
  *          缓冲区未满时会直接返回原始值
  */
